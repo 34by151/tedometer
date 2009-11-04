2eb7fab531e1cdbeaceb533ead066a127c25f6e4
Nathan Hadfield <hadfieldn@gmail.com>
2009-11-04 06:23:14 +0000

    Added docs for history API, added CorePlot framework to project settings
    git-svn-id: https://tedometer.googlecode.com/svn/trunk@67 0de56964-b9b4-11de-a079-5d186ce21964

--- Tedometer/Source/TedometerAppDelegate.h
+++ Tedometer/Source/TedometerAppDelegate.h
@@ -19,7 +19,6 @@
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) MainViewController *mainViewController;
 @property (retain) NSOperationQueue *sharedOperationQueue;
-- (void)batteryStateDidChange:(NSNotification *) notification;
 
 @end
 

12aac630820226c1c4bf091c3cf789daeb8c8dc9
Nathan Hadfield <hadfieldn@gmail.com>
2009-11-17 19:17:17 +0000

    Added InternetRequiredView for when not connected to WiFi or cellular network.
    git-svn-id: https://tedometer.googlecode.com/svn/trunk@71 0de56964-b9b4-11de-a079-5d186ce21964

--- Tedometer/Source/TedometerAppDelegate.h
+++ Tedometer/Source/TedometerAppDelegate.h
@@ -7,18 +7,27 @@
 //
 
 #import "TedometerData.h"
+#import "Reachability.h"
 
 @class MainViewController;
+@class InternetRequiredViewController;
 
 @interface TedometerAppDelegate : NSObject <UIApplicationDelegate> {
     UIWindow *window;
     MainViewController *mainViewController;
+    InternetRequiredViewController *internetRequiredViewController;
 	NSOperationQueue *sharedOperationQueue;
+	
+    Reachability* internetReach;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) MainViewController *mainViewController;
+@property (nonatomic, retain) InternetRequiredViewController *internetRequiredViewController;
 @property (retain) NSOperationQueue *sharedOperationQueue;
+
+- (void) updateInterfaceWithReachability: (Reachability*) curReach;
+- (void) reachabilityChanged: (NSNotification* )note;
 
 @end
 
--- Tedometer/Source/TedometerAppDelegate.m
+++ Tedometer/Source/TedometerAppDelegate.m
@@ -8,13 +8,14 @@
 
 #import "TedometerAppDelegate.h"
 #import "MainViewController.h"
-#import "TedometerData.h"
+#import "InternetRequiredViewController.h"
 
 @implementation TedometerAppDelegate
 
 
 @synthesize window;
 @synthesize mainViewController;
+@synthesize internetRequiredViewController;
 @synthesize sharedOperationQueue;
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
@@ -23,17 +24,50 @@
 	self.sharedOperationQueue = opQueue;
 	[opQueue release];
 	
-	MainViewController *aController = [[MainViewController alloc] initWithNibName:@"MainView" bundle:nil];
-	self.mainViewController = aController;
-	[aController release];
+	[[NSNotificationCenter defaultCenter] addObserver: self selector: @selector(reachabilityChanged:) name: kReachabilityChangedNotification object: nil];
+    internetReach = [[Reachability reachabilityForInternetConnection] retain];
+	[internetReach startNotifer];
 	
+	UIViewController *aController = [[MainViewController alloc] initWithNibName:@"MainView" bundle:nil];
+	self.mainViewController = (MainViewController*) aController;
+	[aController release];
+
+	aController = [[InternetRequiredViewController alloc] initWithNibName:@"InternetRequiredView" bundle:nil];	
+	self.internetRequiredViewController = (InternetRequiredViewController *) aController;
+
     mainViewController.view.frame = [UIScreen mainScreen].applicationFrame;
+    internetRequiredViewController.view.frame = [UIScreen mainScreen].applicationFrame;
+	
+	[window addSubview:[internetRequiredViewController view]];
 	[window addSubview:[mainViewController view]];
+	
+	[self updateInterfaceWithReachability: internetReach];
+
     [window makeKeyAndVisible];
 
 }
 
 
+- (void) updateInterfaceWithReachability: (Reachability*) curReach
+{
+	if( curReach == internetReach ) {
+		NetworkStatus netStatus = [curReach currentReachabilityStatus];
+		BOOL connectionRequired = [curReach connectionRequired];
+		if( netStatus == NotReachable ) {
+			[window bringSubviewToFront:internetRequiredViewController.view];
+		}
+		else {
+			[window bringSubviewToFront:mainViewController.view];
+		}
+	}
+}
+
+- (void) reachabilityChanged: (NSNotification* )note
+{
+	Reachability* curReach = [note object];
+	NSParameterAssert([curReach isKindOfClass: [Reachability class]]);
+	[self updateInterfaceWithReachability: curReach];
+}
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
@@ -49,6 +83,8 @@
 
 - (void)dealloc {
 	[mainViewController release];
+	[internetRequiredViewController release];
+	[internetReach release];
     [window release];
     [super dealloc];
 }
